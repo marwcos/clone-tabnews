bf11282255ca40e67ab7f0cef229456ee4028b8e
marwcos <134428980+marwcos@users.noreply.github.com>
2023-10-25 19:38:25 +0000

    add `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,7 @@ function Home () {
     return <h1>Flor, amo você! Se você me ama, dá uma risadinha!</h1>
     }
     
+    function teste () {
+      console.log ('teste');
+    }
     export default Home;
